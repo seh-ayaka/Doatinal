1f310dcf54a3b4e7bc83f1776076e64da032a983
serena <serena.tsuchiya@aluno.ifsp.edu.br>
2023-08-16 09:24:45 -0300

    inclui index sobre equipe e templates

--- app/views.py
+++ app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+def index (request) :
+    return render (request, "app/index.html" )
+
+def sobre (request) :
+    return render (request, "app/sobre.html" )
+
+def equipe (request) :
+    return render (request, "app/equipe.html")
+
 # Create your views here.
