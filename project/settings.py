c58b44c462eafd7fe40f799ca45334a566dd41cd
serena <serena.tsuchiya@aluno.ifsp.edu.br>
2023-08-16 08:11:18 -0300

    "doAtional" correção do nome do banco de dados

--- project/settings.py
+++ project/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Dostionaldb',
+        'NAME': 'Doationaldb',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
