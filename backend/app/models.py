e24625ff5f94d8fefc926c39c77c4730fe269f2a
Matheus Costa <matheusrc0107@gmail.com>
2023-09-12 11:20:33 -0300

    criacao tabela usuario e ajustes nas pastas

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+class Doador(models.Model):
+    cpf = models.CharField(max_length=11, unique=True)
+    nome = models.CharField(max_length=200)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=20, null=True, blank=True)
+    senha = models.CharField(max_length=200, null=True, blank=True)
+    cep = models.BigIntegerField(null=True, blank=True)
+    endereco = models.CharField(max_length=200)
+    rg = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nome
+
+
 # Create your models here.
